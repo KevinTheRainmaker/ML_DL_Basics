c607d4654f7d03cf50324dc764e2aa2a0ae5fc91
Kangbeen Ko <sweetrainforyou@gmail.com>
2022-12-03 19:00:55 +0900

    test(evaluate*.py): copying test case

--- ComputerVision/PA3_Image_Harmonization/PA3/evaluate2.py
+++ ComputerVision/PA3_Image_Harmonization/PA3/evaluate2.py
@@ -11,7 +11,7 @@ from skimage import data, io
 from skimage.metrics import mean_squared_error
 from skimage.metrics import peak_signal_noise_ratio
 from util.config import cfg
-from models.networks import RainNet
+from models.networks2 import RainNet
 from models.normalize import RAIN
 from data.dataset import Iharmony4Dataset
 
@@ -19,18 +19,18 @@ from data.dataset import Iharmony4Dataset
 def calculateMean(vars):
     return sum(vars) / len(vars)
 
-def load_network(cfg):
-    net = RainNet(input_nc=cfg.input_nc, 
-                output_nc=cfg.output_nc, 
-                ngf=cfg.ngf, 
-                norm_layer=RAIN, 
-                use_dropout=not cfg.no_dropout)
 
+def load_network(cfg):
+    net = RainNet(input_nc=cfg.input_nc,
+                  output_nc=cfg.output_nc,
+                  ngf=cfg.ngf,
+                  norm_layer=RAIN,
+                  use_dropout=not cfg.no_dropout)
 
     # change the path to your own checkpoint path
     load_path = os.path.join(cfg.checkpoints_dir, cfg.name, 'latest_net_G.pth')
 
-    #if not os.path.exists(load_path):
+    # if not os.path.exists(load_path):
     #    raise FileExistsError, print('%s not exists. Please check the file'%(load_path))
     print(f'loading the model from {load_path}')
     state_dict = torch.load(load_path)
@@ -38,34 +38,44 @@ def load_network(cfg):
     # net.load_state_dict(state_dict)
     return net
 
+
 def save_img(path, img):
     fold, name = os.path.split(path)
     os.makedirs(fold, exist_ok=True)
     io.imsave(path, img)
 
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--dataset_root', default='none', required=False, type=str, help='the path of the dataset for evaluation')
-    parser.add_argument('--batch_size', default=16, required=False, type=int, help='batchsize of inference')
-    parser.add_argument('--save_dir', default='evaluate', required=False, type=str, help='directory to save evaluating results')
-    parser.add_argument('--store_image', action='store_true', required=False, help='whether store the result images')
-    parser.add_argument('--device', default='cuda', type=str, help='device to running the code | default cuda')
+    parser.add_argument('--dataset_root', default='none', required=False,
+                        type=str, help='the path of the dataset for evaluation')
+    parser.add_argument('--batch_size', default=16,
+                        required=False, type=int, help='batchsize of inference')
+    parser.add_argument('--save_dir', default='evaluate', required=False,
+                        type=str, help='directory to save evaluating results')
+    parser.add_argument('--store_image', action='store_true',
+                        required=False, help='whether store the result images')
+    parser.add_argument('--device', default='cuda', type=str,
+                        help='device to running the code | default cuda')
     user_args = parser.parse_args()
     if user_args.dataset_root is not 'none':
         cfg.dataset_root = user_args.dataset_root
 
     # ----------------- main --------------------
     device = user_args.device
-    assert device.startswith('cpu') or device.startswith('cuda'), 'Device setting error. Please check --device in the arguments'
+    assert device.startswith('cpu') or device.startswith(
+        'cuda'), 'Device setting error. Please check --device in the arguments'
 
     testdata = Iharmony4Dataset(cfg, is_for_train=False)
-    testloader = DataLoader(testdata, batch_size=user_args.batch_size, shuffle=False, drop_last=False)
+    testloader = DataLoader(
+        testdata, batch_size=user_args.batch_size, shuffle=False, drop_last=False)
     net = load_network(cfg)
     net = net.to(device)
     net.eval()
 
     os.makedirs(user_args.save_dir, exist_ok=True)
-    fsave_results = open(os.path.join(user_args.save_dir, 'test_results.csv'), 'w')
+    fsave_results = open(os.path.join(
+        user_args.save_dir, 'test_results.csv'), 'w')
     fsave_results.writelines('image_path,foreground_ratio,MSE,PSNR\n')
     all_psnr, all_mse = [], []
     for i, batch_data in enumerate(tqdm(testloader)):
@@ -75,7 +85,8 @@ if __name__ == '__main__':
         pred = net.processImage(comp, mask)
         for img_idx in range(comp.size(0)):
             img_path = str(batch_data['img_path'][img_idx])
-            comp_rgb = util.tensor2im(comp[img_idx:img_idx+1]) # input shape should be (1, 3, H, W) or (1, 1, H, W)
+            # input shape should be (1, 3, H, W) or (1, 1, H, W)
+            comp_rgb = util.tensor2im(comp[img_idx:img_idx+1])
             pred_rgb = util.tensor2im(pred[img_idx:img_idx+1])
             mask_rgb = util.tensor2im(mask[img_idx:img_idx+1])
             real_rgb = util.tensor2im(real[img_idx:img_idx+1])
@@ -84,16 +95,17 @@ if __name__ == '__main__':
             all_psnr.append(psnr_score_op)
             all_mse.append(mse_score_op)
 
-            fsave_results.writelines('%s,%.2f,%.2f,%.2f\n' % (img_path, mask[img_idx].mean().item(), mse_score_op, psnr_score_op))
+            fsave_results.writelines('%s,%.2f,%.2f,%.2f\n' % (
+                img_path, mask[img_idx].mean().item(), mse_score_op, psnr_score_op))
             if user_args.store_image:
                 basename, imagename = os.path.split(img_path)
-                basename = basename.split('/')[-2] # HAdobe, HCOCO etc...
-                save_img(os.path.join(user_args.save_dir, basename, imagename.split('.')[0] + '.png'), 
-                        np.hstack([comp_rgb, mask_rgb, real_rgb, pred_rgb]))
-            
+                basename = basename.split('/')[-2]  # HAdobe, HCOCO etc...
+                save_img(os.path.join(user_args.save_dir, basename, imagename.split('.')[0] + '.png'),
+                         np.hstack([comp_rgb, mask_rgb, real_rgb, pred_rgb]))
+
         if i+1 % 50 == 0:
             fsave_results.flush()
-    print('PSNR: %.4f  MSE: %.4f' % (calculateMean(all_psnr), calculateMean(all_mse)))
+    print('PSNR: %.4f  MSE: %.4f' %
+          (calculateMean(all_psnr), calculateMean(all_mse)))
     fsave_results.flush()
     fsave_results.close()
-    
